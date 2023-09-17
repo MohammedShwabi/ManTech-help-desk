66cd305388e9d0e2e9bd4fcff0da9cf023cb53a0
osama-nasser1999 <129899937+osama-nasser1999@users.noreply.github.com>
2023-09-17 15:48:59 +0300

    header not complated and footer

--- Front-end/js/frontend.js
+++ Front-end/js/frontend.js
@@ -6,3 +6,14 @@ function getYear() {
 }
 
 getYear();
+
+$('.hamburger').click(function (e) {
+    $(this).parent().find('ol').toggleClass('active');
+    $(this).toggleClass('active');
+    $('.dimmer').toggleClass('active');
+    $('body').toggleClass('no-scrolling');
+});
+
+$('li .icon-mobileMenu').click(function () {
+    $(this).parent().toggleClass('active');
+});
