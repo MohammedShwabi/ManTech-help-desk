2d3b15b668867167b4e7971145c36860b77659d0
Mohammed Nabeel Shwabi <85250958+MohammedShwabi@users.noreply.github.com>
2023-09-21 22:02:48 +0300

    complete style for side bar

--- Front-end/js/frontend.js
+++ Front-end/js/frontend.js
@@ -1,19 +1,18 @@
-// to get current year
-function getYear() {
-    var currentDate = new Date();
-    var currentYear = currentDate.getFullYear();
-    document.querySelector("#displayYear").innerHTML = currentYear;
-}
+// to collapse side bar
+document.addEventListener("DOMContentLoaded", function() {
+    // Get all the collapsible buttons
+    const buttons = document.querySelectorAll(".side-header");
 
-getYear();
+    // Add a click event listener to each button
+    buttons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            // Find the associated icon elements within this section
+            const openIcon = this.querySelector(".open-icon");
+            const closeIcon = this.querySelector(".close-icon");
 
-$('.hamburger').click(function (e) {
-    $(this).parent().find('ol').toggleClass('active');
-    $(this).toggleClass('active');
-    $('.dimmer').toggleClass('active');
-    $('body').toggleClass('no-scrolling');
-});
-
-$('li .icon-mobileMenu').click(function () {
-    $(this).parent().toggleClass('active');
+            // Toggle the visibility of the icons
+            openIcon.style.display = openIcon.style.display === "none" ? "inline-block" : "none";
+            closeIcon.style.display = closeIcon.style.display === "none" ? "inline-block" : "none";
+        });
+    });
 });
