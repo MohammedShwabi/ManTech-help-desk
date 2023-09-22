7a0977b4946b11627a0276d9b89ea710bc6d0651
Hesham Mohammed Noaman <140953153+HeshamNoaman@users.noreply.github.com>
2023-09-23 02:02:56 +0300

    edit and add in same category page

--- Back-end/mantech/src/java/controller/CategoryManagedBean.java
+++ Back-end/mantech/src/java/controller/CategoryManagedBean.java
@@ -25,6 +25,9 @@ public class CategoryManagedBean implements Serializable {
     private CategoriesFacade categoriesFacade;
     private Categories categories = new Categories();
 
+    // The category being edited
+    private Categories editingCategory = null;
+
     public Categories getCategories() {
         return categories;
     }
@@ -32,25 +35,71 @@ public class CategoryManagedBean implements Serializable {
     public void setCategories(Categories categories) {
         this.categories = categories;
     }
-    
-    
-    
+
+    public Categories getEditingCategory() {
+        return editingCategory;
+    }
+
+    public void setEditingCategory(Categories editingCategory) {
+        this.editingCategory = editingCategory;
+    }
+
+    // checks if a category is currently being edited
+    public boolean isEditing(Categories category) {
+        return category.equals(editingCategory);
+    }
+
+    // start editing a category
+    public void startEditing(Categories category) {
+        editingCategory = category;
+    }
+
+    // stop editing a category
+    public void stopEditing() {
+        editingCategory = null;
+    }
+
+    // update the edited category
+    public void updateCategory() {
+        categoriesFacade.edit(editingCategory);
+        stopEditing();
+    }
+
+    // cancel editing
+    public void cancelEdit() {
+        stopEditing();
+    }
+
+    // handles the logic for the "Update/OK" button
+    public void checkUpdate(Categories cat) {
+        if (isEditing(cat)) {
+            updateCategory(); // If already editing, update the category
+        } else {
+            startEditing(cat); // If not editing, start editing this category
+        }
+    }
 
     /**
      * Creates a new instance of CategoryManagedBean
      */
     public CategoryManagedBean() {
     }
-    
+
     // for get all categories data
     public List<Categories> findAll() {
         return categoriesFacade.findAll();
     }
-    
-    // to delete an faq
+
+    // to delete an category
     public void delete(Categories categories) {
         categoriesFacade.remove(categories);
     }
-    
-    
+
+    // to add a new category
+    public void addCategory() {
+        categoriesFacade.create(categories);
+        // Clear the input field
+        categories = new Categories();
+    }
+
 }
